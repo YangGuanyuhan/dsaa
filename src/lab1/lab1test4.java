bc0e12c93e0536f128e2169e8ace89828337eff6
Christy <1523543870@qq.com>
2024-09-13 11:46:58 +0800

    4

--- src/lab1/lab1test4.java
+++ src/lab1/lab1test4.java
@@ -1,7 +1,44 @@
 package lab1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class lab1test4 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+
+
+            extracted(in);
+        }
+
+    }
+
+    private static void extracted(Scanner in) {
+        int n = in.nextInt();
+        int[] arr1 = new int[n + 1];
+        int[] difference = new int[n + 1];
+        difference[0] = Integer.MIN_VALUE;
+        difference[n] = Integer.MIN_VALUE;
+
+        for (int i = 1; i < n + 1; i++) {
+            arr1[i] = in.nextInt();
+        }
+        int min = arr1[n];
+        for (int i = n; i > 1; i--) {
+            difference[i - 1] = arr1[i - 1] - min;
+            if (arr1[i - 1] < min) {
+                min = arr1[i - 1];
+            }
+        }
+        int max = difference[0];
+        for (int i = 1; i < difference.length; i++) {
+            if (difference[i] > max) {
+                max = difference[i];
+            }
+        }
+
+        System.out.println(max);
     }
 }
