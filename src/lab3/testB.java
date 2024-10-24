7591160b6ff00b672ec912892041a6d88664b98c
Christy <1523543870@qq.com>
2024-10-24 23:56:04 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/lab2/testF5_0.java
    #	src/lab2/testF6_0.java

--- src/lab3/testB.java
+++ src/lab3/testB.java
@@ -8,31 +8,56 @@ import java.util.Scanner;
 public class testB {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
+        int t = in.nextInt();
         for (int i = 0; i < t; i++) {
             int n = in.nextInt();
-            node nodhead = new node(0,-1);
+            node nodhead = new node(0, -1);
             node current = nodhead;
-            for (int j = 0; j < in.nextInt(); j++) {
-                node temp = new node(in.nextInt(),in.nextInt());
-                temp =temp.next;
-
-
-
-
-
+            for (int j = 0; j < n; j++) {
+                node temp = new node(in.nextInt(), in.nextInt());
+                current.next = temp;
+                current = current.next;
             }
-
-
+            node tail = new node(0, Integer.MAX_VALUE);
+            current.next = tail;
+            current = nodhead;
+            int m = in.nextInt();
+            for (int j = 0; j < m; j++) {
+                int coe = in.nextInt();
+                int exp = in.nextInt();
+                while (current.next.expotential <= exp) {
+                    current = current.next;
+                }
+                if (current.expotential == exp) {
+                    current.coeffient += coe;
+                } else {
+                    node temp = new node(coe, exp);
+                    temp.next = current.next;
+                    current.next = temp;
+                }}
+                int szdihjfbgv = in.nextInt();
+                current = nodhead.next;
+                for (int k = 0; k < szdihjfbgv; k++) {
+                    int x = in.nextInt();
+                    while (current.next.expotential <= x) {
+                        current = current.next;
+                    }
+                    if (current.expotential == x) {
+                        System.out.print(current.coeffient + " ");
+                    } else {
+                        System.out.print("0 ");
+                    }
+                }
 
 
         }
-    }
 
+    }
 }
-class node{
-    private int coeffient;
-    private int expotential;
+
+class node {
+    int coeffient;
+    int expotential;
     node next;//存储的地址值
 
     public node(int coeffient, int expotential) {
