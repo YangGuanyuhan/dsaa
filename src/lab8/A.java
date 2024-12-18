ad187460fa2f9ddcab1c70c9a2774682a2b2ceef
Christy <1523543870@qq.com>
2024-12-18 17:51:20 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/lab2/testF5_0.java
    #	src/lab2/testF6_0.java

--- src/lab8/A.java
+++ src/lab8/A.java
@@ -1,6 +1,9 @@
+package lab8;
+
 import java.util.Scanner;
 
-public class Main {
+
+public class A {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
