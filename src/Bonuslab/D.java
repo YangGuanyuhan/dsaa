91a035332176e71b0864366468449029ff537b8a
Christy <1523543870@qq.com>
2024-12-24 21:09:30 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/lab2/testF5_0.java
    #	src/lab2/testF6_0.java

--- src/Bonuslab/D.java
+++ src/Bonuslab/D.java
@@ -1,6 +1,8 @@
+package Bonuslab;
+
 import java.util.Scanner;
 
-public class Main {
+public class D {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
